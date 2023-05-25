c3d05f7c939bf5970d58319949a2b7978dd74c4e
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2023-05-25 11:24:02 +0900

    [add]プレイヤーの動きを追加

---
+++ Assets/Mikuto/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float m_speed = 1f;
+    [SerializeField] float m_jumpPower = 1f;
+    Rigidbody2D m_rb = default;
+    float m_horizontal;
+    bool isGrounded = false;
+    bool isJumping = false;
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        m_horizontal = Input.GetAxisRaw("Horizontal");
+        m_rb.AddForce(Vector2.right * m_horizontal * m_speed, ForceMode2D.Force);
+
+        if (Input.GetButtonDown("Jump") && (isGrounded || isJumping))
+        {
+            m_rb.AddForce(Vector2.up * m_jumpPower, ForceMode2D.Impulse);
+            if (isJumping)
+            {
+                isJumping = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+        isJumping = true;
+    }
+}
