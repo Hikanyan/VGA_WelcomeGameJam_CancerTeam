8fd9c20724f05d40f1cae4a82c9517c9de7850a5
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-05-25 12:01:54 +0900

    [Add] ScoreManager TimerManager [Update] GameManager

--- Assets/Hikanyan_Assets/Script/GameManager.cs
+++ Assets/Hikanyan_Assets/Script/GameManager.cs
@@ -1,8 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using Cysharp.Threading.Tasks;
 
 public class GameManager : AbstractSingleton<GameManager>
 {
-    
+    enum GameState
+    {
+        Title,
+        InGame,
+        Result,
+        Explanation
+    }
+
+    private ScoreManager scoreManager;
+    private TimerManager timerManager;
+
+    private void Start()
+    {
+        scoreManager = new ScoreManager();
+        timerManager = new TimerManager();
+    }
+
+    public void AddScore(int points)
+    {
+        scoreManager.AddScore(points);
+    }
+
+    public void ResetScore()
+    {
+        scoreManager.ResetScore();
+    }
+
+    public async UniTask StartTimer(float duration)
+    {
+        await timerManager.StartTimer(duration);
+    }
+
+    public void ResetTimer()
+    {
+        timerManager.ResetTimer();
+    }
 }
---
+++ Assets/Hikanyan_Assets/Script/TimerManager.cs
@@ -0,0 +1,27 @@
+using UniRx;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+public class TimerManager
+{
+    private FloatReactiveProperty _timer = new FloatReactiveProperty(0f);
+
+    public IReadOnlyReactiveProperty<float> Timer => _timer;
+
+    public async UniTask StartTimer(float duration)
+    {
+        _timer.Value = duration;
+
+        while (_timer.Value > 0f)
+        {
+            await UniTask.DelayFrame(1);
+            _timer.Value -= Time.deltaTime;
+        }
+
+        _timer.Value = 0f;
+    }
+
+    public void ResetTimer()
+    {
+        _timer.Value = 0f;
+    }
+}
